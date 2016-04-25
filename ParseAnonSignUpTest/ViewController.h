8ea73de01d296aa3ddf79e7c379763bd0c1d2e2a
Calvin Yu <me@sourcebender.com>
2016-04-25 11:34:20 -0400

    Initial Commit

---
+++ ParseAnonSignUpTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParseAnonSignUpTest
+//
+//  Created by Calvin Yu on 4/25/16.
+//  Copyright © 2016 BlueboardMedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ParseAnonSignUpTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ParseAnonSignUpTest
+//
+//  Created by Calvin Yu on 4/25/16.
+//  Copyright © 2016 BlueboardMedia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
